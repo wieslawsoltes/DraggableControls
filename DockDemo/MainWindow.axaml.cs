b29a31e17ecbf0dcf033f39e78416eb2b1b5ab9a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-02-11 22:19:32 +0100

    Add binding to Items sample

--- DockDemo/MainWindow.axaml.cs
+++ DockDemo/MainWindow.axaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -6,9 +8,23 @@ namespace DockDemo
 {
     public class MainWindow : Window
     {
+        public IList<string> Items { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Items = new ObservableCollection<string>()
+            {
+                "Item1",
+                "Item2",
+                "Item3",
+                "Item4",
+                "Item5",
+                "Item6",
+                "Item7",
+                "Item8"
+            };
+            DataContext = this;
 #if DEBUG
             this.AttachDevTools();
 #endif
