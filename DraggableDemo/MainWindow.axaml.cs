17daca65c665ff205cf6d70540d8b75741997ef5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-06 22:43:59 +0200

    Add Item view model

--- DraggableDemo/MainWindow.axaml.cs
+++ DraggableDemo/MainWindow.axaml.cs
@@ -8,21 +8,21 @@ namespace DraggableDemo
 {
     public class MainWindow : Window
     {
-        public IList<string> Items { get; set; }
+        public IList<Item> Items { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            Items = new ObservableCollection<string>()
+            Items = new ObservableCollection<Item>()
             {
-                "Item1",
-                "Item2",
-                "Item3",
-                "Item4",
-                "Item5",
-                "Item6",
-                "Item7",
-                "Item8"
+                new () { Title = "Item1", X = 30, Y = 30 },
+                new () { Title = "Item2", X = 90, Y = 30 },
+                new () { Title = "Item3", X = 120, Y = 60 },
+                new () { Title = "Item4", X = 45, Y = 90 },
+                new () { Title = "Item5", X = 60, Y = 120 },
+                new () { Title = "Item6", X = 150, Y = 180 },
+                new () { Title = "Item7", X = 250, Y = 120 },
+                new () { Title = "Item8", X = 300, Y = 150 }
             };
             DataContext = this;
 #if DEBUG
